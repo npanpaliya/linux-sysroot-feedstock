2500fbb243ddd52a05f5a70644d6e4915768b5ab
Ray Donnelly <mingw.android@gmail.com>
2021-01-27 11:44:12 +0100

    Updates for aarch64 and ppc64le fixes

--- recipe/build-sysroot.sh
+++ recipe/build-sysroot.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+set -x
+
 mkdir -p ${PREFIX}/${target_machine}-${ctng_vendor}-linux-gnu/sysroot
 pushd ${PREFIX}/${target_machine}-${ctng_vendor}-linux-gnu/sysroot > /dev/null 2>&1
 cp -Rf "${SRC_DIR}"/binary/* .
 mkdir -p usr/include
-cp -Rf "${SRC_DIR}"/binary-tzdata/* usr/
-cp -Rf "${SRC_DIR}"/binary-glibc-headers/usr/include/* usr/include/
-cp -Rf "${SRC_DIR}"/binary-glibc-devel/* usr/
+cp -Rf "${SRC_DIR}"/binary-tzdata/* .
+cp -Rf "${SRC_DIR}"/binary-glibc-headers/usr .
+cp -Rf "${SRC_DIR}"/binary-glibc-devel/* .
 cp -Rf "${SRC_DIR}"/binary-glibc-common/* .
 
 mkdir -p usr/lib
@@ -16,8 +18,19 @@ rm -rf usr/lib
 ln -s $PWD/usr/lib64 $PWD/usr/lib
 
 if [ -d "lib" ]; then
-    mv lib/* lib64/
-    rm -rf lib
+  pushd lib
+    declare -a FILES=($(ls -1))
+  popd
+  # Takes care of the case when lib/ld-linux-aarch64.so.1 is a symlink to ../lib64/ld-2.17.so:
+  # mv: lib/ld-linux-aarch64.so.1 and lib64/ld-linux-aarch64.so.1 are identical
+  for FILE in "${FILES[@]}"; do
+    if [[ ! -f lib64/${FILE} ]] && [[ ! -L lib64/${FILE} ]]; then
+      mv lib/${FILE} lib64
+    else
+      echo "INFO :: Skipped moving lib/${FILE} to lib64/${FILE} as it exists already"
+    fi
+  done
+  rm -rf lib
 fi
 
 if [[ "$target_machine" == "s390x" ]]; then
